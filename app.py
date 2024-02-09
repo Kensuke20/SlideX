33ee3bfe41191205ca5d503cbe71aaf7686ec6db
Kensuke ONO <daybeianjie07@gmail.com>
2024-02-09 12:48:46 +0900

    new file:   app.py new file:   static/logo.css new file:   templates/base.html new file:   templates/index.html new file:   templates/photo_page.html new file:   templates/upload_page.html

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, redirect, request
+from datetime import datetime
+import glob, os
+
+app = Flask(__name__)
+
+# top page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/photo_page')
+def photo_page():
+    image_urls = glob.glob("./static/images/*.jpg")
+    st_birthtime = os.stat(image_urls[0]).st_ctime      # remove later
+    birth_time = datetime.fromtimestamp(st_birthtime)   # remove later
+    return render_template('photo_page.html', image_urls=image_urls, time=birth_time)
+
+
+@app.route('/upload_page')
+def upload_page():
+    return render_template('upload_page.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_try():
+    upfile = request.files['upfile']
+    fname = datetime.now().strftime('%Y%m%d%H%M%S') + '.jpg'
+
+    upfile.save('/static/images/' + fname)
+
+    return redirect('/photo')
+
+
+
+# Automatically add version after static file
+@app.context_processor
+def add_staticfile():
+    return dict(staticfile=staticfile_cp)
+def staticfile_cp(fname):
+    import os
+    path = os.path.join(app.root_path, 'static', fname)
+    mtime = str(int(os.stat(path).st_mtime))
+    return '/static/' + fname + '?v=' + str(mtime)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
